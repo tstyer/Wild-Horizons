d8c998235aac7c7eb33fc99e0a7f6d2cf136f52c
tstyer <travisstyer.ts@gmail.com>
2026-03-04 10:40:30 +0000

    Add: data.js

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 import http from 'node:http'
 
 
-const port  = 8000
+const port = 8000
 
 /* Accessing the server method */
 /* Takes requests (req), and responds (res) */
+
 const server = http.createServer((req, res) => {
-    res.end("testing")
+
+    if(req.url === '/' && req.method === 'GET') {
+        res.end("Connected")
+    }
+
+    res.end("End Response")
 })
 
-server.listen(() => console.log(`server running on port: ${port}`))
-
+server.listen(() => console.log(`Server running on port: ${port}`))
 
 
 /* LEARNING NOTES:
     end() method sends data over the http and then ends the response. 
     To use the server, you need to specify a port (8000, for example).
+
+    When an outside source send a request to port 8000, then the server will repsond with "test"
+    since that is what is listed as a res.end.
+
+    Use an if statement to dictate whether your server will respond to certain requests from specific url's. 
 */
