59e9cf0e15088ad638ed7a150165048b5234f44c
tstyer <travisstyer.ts@gmail.com>
2026-03-04 11:08:51 +0000

    Change req.url to only be for '/api'

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = 8000
 const server = http.createServer(async (req, res) => {
   const destinations = await getDataFromDB()
 
-    if(req.url === '/' && req.method === 'GET') {
+    if(req.url === '/api' && req.method === 'GET') {
         res.statusCode = 200
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify(destinations))
